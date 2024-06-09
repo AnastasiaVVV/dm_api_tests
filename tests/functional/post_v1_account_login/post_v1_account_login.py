b2b24391f69364885174669e3539b3864bdf112e
Anastasia <nastyamorygova@mail.ru>
2024-06-09 12:44:21 +0400

    переименованы тесты внутри файлов, чтобы запускались в один прогон

--- tests/functional/post_v1_account_login/post_v1_account_login.py
+++ tests/functional/post_v1_account_login/post_v1_account_login.py
@@ -5,12 +5,12 @@ from dm_api_account.apis.login_api import LoginApi
 from api_mailhog.apis.mailhog_api import MailhogApi
 
 
-def test_post_v1_account():
+def test_post_v1_account_login():
     # Регистрация пользователя
     account_api = AccountApi(host='http://5.63.153.31:5051')
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
-    login = 'morugova_test4__0543_45'
+    login = 'morugova_test4090045'
     password = '123456789'
     email = f'{login}@mail.ru'
     json_data = {
--- tests/functional/put_v1_account_email/put_v1_account_email.py
+++ tests/functional/put_v1_account_email/put_v1_account_email.py
@@ -7,12 +7,12 @@ from api_mailhog.apis.mailhog_api import MailhogApi
 
 
 
-def test_post_v1_account():
+def test_post_v1_account_email():
     # Регистрация пользователя
     account_api = AccountApi(host='http://5.63.153.31:5051')
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
-    login = 'morugova_test34523___45'
+    login = 'morugova_test3409__45'
     password = '123456789'
     email = f'{login}@mail.ru'
     json_data = {
